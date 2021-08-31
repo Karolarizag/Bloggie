dec2bcd49685766e724633f27c9115a2ad79344b
Karolarizag <karolarizag@gmail.com>
2021-08-31 16:29:38 +0100

    name

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - bloggie',
+    titleTemplate: '%s',
     title: 'bloggie',
     meta: [
       { charset: 'utf-8' },
